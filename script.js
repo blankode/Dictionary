63bad3f869ecfde33f6455627e4de02ab07521b8
blankode <porosnicu.bogdan@outlook.com>
2021-11-16 15:39:39 +0100

    identation fix

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function addWord() {
     const dictionaryWord = localStorage.getItem(saveWord);
     if (saveWord !== dictionaryWord) {
         document.getElementById("notification").innerHTML = `<div class="alert alert-warning" role="alert">
-        Word <strong>`+ saveWord + `</strong> has been added to the dictionary!
+        Word <strong>` + saveWord + `</strong> has been added to the dictionary!
         </div>`
         localStorage.setItem(saveWord, saveWord);
-        setTimeout(function(){document.getElementById("notification").innerHTML = "";}, 3000);
+        setTimeout(function () {
+            document.getElementById("notification").innerHTML = "";
+        }, 3000);
     } else {
         document.getElementById("notification").innerHTML = `<div class="alert alert-danger" role="alert">
-        The word <strong>`+ saveWord + `</strong> already exists in the dictionary..
+        The word <strong>` + saveWord + `</strong> already exists in the dictionary..
         </div>`
-        setTimeout(function(){document.getElementById("notification").innerHTML = "";}, 3000);
+        setTimeout(function () {
+            document.getElementById("notification").innerHTML = "";
+        }, 3000);
     }
     document.getElementById("getWord").value = "";
 }
@@ -21,14 +25,18 @@ function searchWord() {
     const dictionaryWord = localStorage.getItem(word);
     if (word === dictionaryWord) {
         document.getElementById("notification").innerHTML = `<div class="alert alert-success" role="alert">
-        Word <strong>`+ word + `</strong> has been found!
+        Word <strong>` + word + `</strong> has been found!
         </div>`
-        setTimeout(function(){document.getElementById("notification").innerHTML = "";}, 3000);
+        setTimeout(function () {
+            document.getElementById("notification").innerHTML = "";
+        }, 3000);
     } else {
         document.getElementById("notification").innerHTML = `<div class="alert alert-danger" role="alert">
-        The word <strong>`+ word + `</strong> was not found..
+        The word <strong>` + word + `</strong> was not found..
         </div>`
-        setTimeout(function(){document.getElementById("notification").innerHTML = "";}, 3000);
+        setTimeout(function () {
+            document.getElementById("notification").innerHTML = "";
+        }, 3000);
     }
     document.getElementById("checkWord").value = "";
 }
@@ -38,5 +46,7 @@ function resetDictionary() {
     document.getElementById("notification").innerHTML = `<div class="alert alert-secondary" role="alert">
     Dictionary library has been reset!
     </div>`
-    setTimeout(function(){document.getElementById("notification").innerHTML = "";}, 3000);
+    setTimeout(function () {
+        document.getElementById("notification").innerHTML = "";
+    }, 3000);
 }
